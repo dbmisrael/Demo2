77029141634a7cf418ca97c56d48bdff364324f8
dbmisrael <78854344+dbmisrael@users.noreply.github.com>
2021-03-21 10:58:49 +0200

    Update dbo.Table_2.sql

--- Tables/dbo.Table_2.sql
+++ Tables/dbo.Table_2.sql
@@ -1,5 +1,6 @@
 CREATE TABLE [dbo].[Table_2]
 (
-[c1] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[c1] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[c3] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
