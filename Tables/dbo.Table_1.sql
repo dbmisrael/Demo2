f180f795e1deb8d396c7f297f52fa85bfe751e55
dbmisrael <78854344+dbmisrael@users.noreply.github.com>
2024-01-15 12:12:25 +0200

    Delete Tables directory

--- Tables/dbo.Table_1.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[Table_1]
-(
-[c2] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[c3] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[c4] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-) ON [PRIMARY]
-GO
--- Tables/dbo.Table_3.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[Table_3]
-(
-[c2] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[c3] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-) ON [PRIMARY]
-GO
